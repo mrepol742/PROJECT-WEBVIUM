2e70fe709d65e393c0507bb4cda68e40813e321a
mrepol742 <mrepol742@protonmail.com>
2021-06-04 09:34:37 +0800

    Fixed Github Build Failed

---
+++ app/src/main/java/com/mrepol742/webvium/net/Ping.java
@@ -0,0 +1,62 @@
+/*
+ *
+ * Copyright (c) 2021 Melvin Jones Repol (mrepol742.github.io). All rights reserved.
+ *
+ * License under the GNU General Public License, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * Unless required by the applicable law or agreed in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mrepol742.webvium.net;
+
+import com.mrepol742.webvium.annotation.release.Keep;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.URL;
+import java.net.UnknownHostException;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class Ping {
+
+    @Keep
+    private Ping() {}
+
+    public static boolean isReachable(String url) {
+        try {
+            if (url.startsWith("http://")) {
+                HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+                connection.setRequestMethod("HEAD");
+                int responseCode = connection.getResponseCode();
+                if (responseCode == 200) {
+                    return true;
+                }
+            } else if (url.startsWith("https://")) {
+                HttpsURLConnection connection = (HttpsURLConnection) new URL(url).openConnection();
+                connection.setRequestMethod("HEAD");
+                int responseCode = connection.getResponseCode();
+                if (responseCode == 200) {
+                    return true;
+                }
+            }
+        } catch (Exception en) {
+            en.printStackTrace();
+        }
+        return false;
+    }
+
+    public static boolean isHostReachable(String url) throws IOException {
+        InetAddress inetAddress = InetAddress.getByName(url);
+        return inetAddress.isReachable(5000);
+    }
+}
