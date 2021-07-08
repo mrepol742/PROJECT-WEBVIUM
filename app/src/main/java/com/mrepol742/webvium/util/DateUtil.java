38b23c82cd95ea7a0fde1fe22b3b317a107158b4
mrepol742 <mrepol742@protonmail.com>
2021-07-08 21:14:10 +0800

    Initial commit

---
+++ app/src/main/java/com/mrepol742/webvium/util/DateUtil.java
@@ -0,0 +1,58 @@
+/*
+ *
+ * Copyright (c) 2021 Melvin Jones Repol (mrepol742.github.io). All rights reserved.
+ *
+ * License under the GNU General Public License, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * Unless required by the applicable law or agreed in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mrepol742.webvium.util;
+
+import com.mrepol742.webvium.annotation.Keep;
+
+public class DateUtil {
+
+    @Keep
+    private DateUtil() {
+
+    }
+
+    public static String format(int sg) {
+        switch (sg) {
+            case 1:
+            return "Jan";
+            case 2:
+                return "Feb";
+            case 3:
+                return "Mar";
+            case 4:
+                return "Apr";
+            case 5:
+                return "May";
+            case 6:
+                return "Jun";
+            case 7:
+                return "Jul";
+            case 8:
+                return "Aug";
+            case 9:
+                return "Sep";
+            case 10:
+                return "Oct";
+            case 11:
+                return "Nov";
+            case 12:
+                return "Dec";
+        }
+        return "";
+    }
+}
